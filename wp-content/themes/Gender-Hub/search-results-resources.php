9084093f5804adfa8d1549c262c0046488418c10
Sarah <sarah@slikkr.com>
2017-03-23 11:23:04 +0000

    Updates to many plugins Inclusion of Taxonomy Terms Order plugin Updates to the Gender-Hub theme, including changes to the site and document library search search features Correction of images sizes bug in Featured Content Correction of slider responsiveness

--- wp-content/themes/Gender-Hub/search-results-resources.php
+++ wp-content/themes/Gender-Hub/search-results-resources.php
@@ -11,26 +11,26 @@ get_header(); ?>
 
 <?php if ( have_posts() ) : ?>
 
-			
-			
-		<header class="page-header">
-                	<h1 class="maintitle"><span><?php printf( __( 'Search results for: <strong>%s</strong>', 'twentythirteen' ), get_search_query() ); ?></span></h1>
-		</header>
+    <header class="page-header">
 
-		<span class="count-items"><?php global $wp_query; echo $wp_query->found_posts; ?> items found</span>
-		
-		
-		<?php genderhub_pagination(); ?>
-		
-			<?php /* The loop */ ?>
-			<?php while ( have_posts() ) : the_post(); ?>
-                            <?php get_template_part( 'content', get_post_type() ); ?>
-                            <?php //get_template_part( 'content-ids_documents' ); ?>
-			<?php endwhile; ?>
-<br /><br />
-		<?php genderhub_pagination(); ?>
+        <h1 class="maintitle"><span><?php printf('Search results for: <strong>%s</strong>', get_search_query() ); ?></span></h1>
 
-		<?php else : ?>
-			<?php get_template_part( 'content', 'none' ); ?>
-		<?php endif; ?>
+    </header>
 
+    <span class="count-items"><?php global $wp_query; echo $wp_query->found_posts; ?> items found</span>
+		
+    <?php genderhub_pagination(); ?>
+
+    <?php while ( have_posts() ) : the_post(); ?>
+
+        <?php get_template_part( 'content', get_post_type() ); ?>
+
+    <?php endwhile; ?>
+
+    <?php genderhub_pagination(); ?>
+
+<?php else : ?>
+
+    <?php get_template_part( 'content', 'none' ); ?>
+
+<?php endif; ?>
