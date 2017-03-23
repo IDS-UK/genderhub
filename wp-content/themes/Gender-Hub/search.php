c82837d559e55c1877b9034905960bf667323d4b
Sarah <sarah@slikkr.com>
2017-03-23 11:23:34 +0000

    Updates to many plugins Inclusion of Taxonomy Terms Order plugin Updates to the Gender-Hub theme, including changes to the site and document library search search features Correction of images sizes bug in Featured Content Correction of slider responsiveness

--- wp-content/themes/Gender-Hub/search.php
+++ wp-content/themes/Gender-Hub/search.php
@@ -1,56 +1,34 @@
 <?php
 /**
- * The template for displaying Search Results pages
+ * The template for displaying regular search results (in the site header and regular search results page sidebar)
+ * Results for the search and filter boxes are displayed by search-results.php
  *
- * @package WordPress
- * @subpackage Twenty_Thirteen
- * @since Twenty Thirteen 1.0
  */
 
 get_header(); ?>
 
+    <div class="section group main_content search">
 
+        <div class="col col1_3 sidebar blockheaders purple">
 
+            <?php if ( function_exists( 'dynamic_sidebar' ) ) dynamic_sidebar( 'search-sidebar' ); ?>
 
+        </div><!--/col span_1_of_4-->
 
-<div class="section group main_content">
+        <div class="col col2_3 archive_content padding10">
 
-<div class="col span_3_of_4 archive_content padding10">
+            <?php if (have_posts()) {
 
-<?php if (have_posts()) { 
-    if($_GET['s']=='uwpsfsearchtrg'){
-        if($_GET['uformid']== '1930'){  //123 is your formid
-            include(get_query_template('search-results-resources'));
-        } else {
-            include(get_query_template('search-results-regular'));
-        }
-    } else {
-        include(get_query_template('search-results-regular'));
-    }
-} else {
-    get_template_part( 'content', 'none' ); 
-}
-?>
+                include(get_query_template('search-results-regular'));
 
+            } else {
 
-</div><!--/col span_3_of_4 padding10-->
+                get_template_part( 'content', 'none' );
 
+            } ?>
 
+        </div><!--/col span_3_of_4 padding10-->
 
-
-<div class="col span_1_of_4 sidebar padding10">
-
-<?php if ( function_exists( 'dynamic_sidebar' ) ) dynamic_sidebar( "knowledge-sidebar" ); ?> 
-
-</div><!--/col span_1_of_4-->
-
-
-	
-	
-
-</div><!--/section group-->
-
-
-
+    </div><!--/section group-->
 
 <?php get_footer(); ?>
