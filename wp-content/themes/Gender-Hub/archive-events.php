e419e4ad08442244147478b31639b71ece2ff470
Sarah Cox <admin@slikkr.com>
2017-04-05 23:53:43 +0100

    Events split into current/upcoming and past Date fixed on blogs-_opinions posts in What's Inspiring Us carousel

--- wp-content/themes/Gender-Hub/archive-events.php
+++ wp-content/themes/Gender-Hub/archive-events.php
@@ -4,7 +4,37 @@
  * The template for displaying Events Archive pages
  */
 
-get_header(); ?>
+get_header();
+
+if (isset($_GET['e']) && $_GET['e'] == 'past') :
+    $span = 'past';
+else :
+    $span = 'current';
+endif;
+
+if ($span === 'past') :
+	$compare = '<=';
+    $orderby = 'DESC';
+
+else :
+    $compare = '>=';
+    $orderby = 'ASC';
+endif;
+
+$args = array(
+	'post_type'  => 'events',
+	'relation' => 'AND',
+	'meta_query' => array(
+		array(
+			'key'       => 'end_date',
+			'compare'   => $compare,
+			'value'     => current_time('Ymd'),
+		),
+	),
+	'orderby'   => array( 'end_date' => $orderby )
+);
+$wp_query = new WP_Query( $args );
+?>
 
 <div class="section group main_content">
 
@@ -12,7 +42,7 @@ get_header(); ?>
 		
 		<header class="archive-header">
 
-			<h1 class="archive-title"><span>Events</span></h1>
+			<h1 class="archive-title"><span><?php echo $span === 'past' ? 'Past ' : 'Current and upcoming ' ?>events</span></h1>
 
 		</header>
 
@@ -44,7 +74,21 @@ get_header(); ?>
 
 	</div>
 
-	<div class="col span_1_of_4 sidebar padding10">
+	<div class="col span_1_of_4 sidebar padding10 blockheaders pink">
+
+        <aside class="widget widget_text">
+            <h3 class="widget-title">More events</h3>
+            <div class="textwidget">
+	        <?php
+	        if ($span === 'past') :
+		        echo '<p>See information about <a href="'.home_url().'/build-capacity/events/">current and upcoming events</a></p>';
+	        else :
+		        $link = add_query_arg( 'e', 'past', home_url().'/build-capacity/events/' );
+		        echo '<p>See <a href="'.$link.'">past events</a></p>';
+	        endif;
+	        ?>
+            </div>
+        </aside>
 
 		<?php if ( function_exists( 'dynamic_sidebar' ) ) dynamic_sidebar( "build-capacity-sidebar" ); ?>
 
